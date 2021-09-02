8dabbd82d397fff9c9023bebe74527593b4ba041
the-shahriar <theshahriarislam@gmail.com>
2021-09-02 08:40:20 +0600

    author url added

--- js/script.js
+++ js/script.js
@@ -14,36 +14,37 @@ const searchBook = () => {
 }
 
 const displayBooks = data => {
-    const mainContainer = document.getElementById('main-container');
+    const resultContainer = document.getElementById('result-container');
     // getting total search result
     const totalResult = data.numFound;
-    mainContainer.innerHTML = `
-        <h3 class="text-center">Total Book Found: ${totalResult}</h3>
+    resultContainer.innerHTML = `
+        <h3 class="text-center mb-4">Total Book Found: ${totalResult}</h3>
     `
-    // Clearing previous result
-    mainContainer.textContent = '';
     // getting all books from object
     const books = data.docs;
     // getting field
     const container = document.getElementById('search-result');
+    container.textContent = '';
     books.forEach(book => {
         const url = `https://openlibrary.org/${book.key}`;
+        const thumbnail = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+        const authorUrl = `https://openlibrary.org/authors/${book.author_key}`
         // console.log(result.title);
         const div = document.createElement('div');
         div.classList.add('col-lg-4');
         div.innerHTML = (
         `
         <div class="book">
+        <img src="${thumbnail}" class="img-fluid" alt="...">
           <h4 class="book-title">
             <a href="${url}" target="_blank" rel="noopener">${book.title}</a>
           </h4>
-          <h5 class="book-author">Author name: ${book.author_name}</h5>
-          <h5 class="publisher">Publisher name: ${book.publisher}</h5>
-          <p class="publish-date">First publish on: ${book?.first_publish_year}</p>
+          <h5 class="book-author"><a target="_blank" href="${authorUrl}">Author: ${book?.author_name}</a></h5>
+          <h6 class="publisher">Publisher: ${book?.publisher}</h6>
+          <p class="publish-date">First Publish On: ${book?.first_publish_year}</p>
         </div>
         `
         );
         container.appendChild(div);
-     
     })
 }
