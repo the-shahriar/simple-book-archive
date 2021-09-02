31c6585cfd599c8c282918e22e0c8341c781747c
the-shahriar <theshahriarislam@gmail.com>
2021-09-02 20:01:08 +0600

    some error handling added

--- js/script.js
+++ js/script.js
@@ -17,34 +17,53 @@ const displayBooks = data => {
     const resultContainer = document.getElementById('result-container');
     // getting total search result
     const totalResult = data.numFound;
-    resultContainer.innerHTML = `
-        <h3 class="text-center mb-4">Total Book Found: ${totalResult}</h3>
-    `
-    // getting all books from object
-    const books = data.docs;
-    // getting field
-    const container = document.getElementById('search-result');
-    container.textContent = '';
-    books.forEach(book => {
-        const url = `https://openlibrary.org/${book.key}`;
-        const thumbnail = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
-        const authorUrl = `https://openlibrary.org/authors/${book.author_key}`
-        // console.log(result.title);
-        const div = document.createElement('div');
-        div.classList.add('col-lg-4');
-        div.innerHTML = (
+    if (totalResult == 0) {
+        resultContainer.innerHTML = `<h3 class="text-white bg-danger py-3">Sorry! No, Result Found</h3>`;
+    }
+    else{
+        // getting all books from object
+        const books = data.docs;
+        const booksLength = books.length;
+        resultContainer.innerHTML = `
+            <div class="d-flex gap-3 justify-content-center">
+                <h3 class="text-center mb-4">Total Hits: ${totalResult}</h3>
+                <h3 class="text-center mb-4">Total Books Found: ${booksLength}</h3>
+            </div> 
         `
-        <div class="book">
-        <img src="${thumbnail}" class="img-fluid" alt="...">
-          <h4 class="book-title">
-            <a href="${url}" target="_blank" rel="noopener">${book.title}</a>
-          </h4>
-          <h5 class="book-author"><a target="_blank" href="${authorUrl}">Author: ${book?.author_name}</a></h5>
-          <h6 class="publisher">Publisher: ${book?.publisher}</h6>
-          <p class="publish-date">First Publish On: ${book?.first_publish_year}</p>
-        </div>
-        `
-        );
-        container.appendChild(div);
-    })
+        // getting field
+        const container = document.getElementById('search-result');
+        // cleaing previous data
+        container.textContent = '';
+        // getting single book with loop
+        books.forEach(book => {
+            console.log(book);
+            const cover = book.cover_i;
+            const url = `https://openlibrary.org/${book.key}`;
+            const coverImageUrl = `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`;
+            const authorUrl = `https://openlibrary.org/authors/${book.author_key}`;
+            const noImageFound = `../image/not_found.jpg `;
+            
+            // console.log(result.title);
+            const div = document.createElement('div');
+            div.classList.add('col-lg-3');
+            div.innerHTML = (
+            `
+            <div class="card h-auto">
+                <img style="width: 100%; height: 350px;" src="${cover === undefined?noImageFound:coverImageUrl}">
+                <div class="card-body">
+                    <h4 class="book-title card-text">
+                        <a class="link-dark text-decoration-none" href="${url}" target="_blank" rel="noopener">${book.title}</a>
+                    </h4>
+                    <h5 class="book-author card-text"><a class="link-success" target="_blank" href="${authorUrl}">Author: ${book.author_name}</a></h5>
+                    <h6 class="publisher card-text">Publisher: ${book.publisher}</h6>
+                    <p class="publish-date card-text">First Publish On: ${book?.first_publish_year}</p>
+                </div>
+            </div>
+            `
+            );
+            container.appendChild(div);
+        })
+
+    }
+
 }
